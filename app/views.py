c70688bd92fc29def142a2633aa3a39c685d198c
Aditya sawant <sawantaditya029@gmail.com>
2026-02-02 10:10:17 +0530

    Fix CORS preflight for Angular

--- app/views.py
+++ app/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 from .models import Contact
 from .serializer import ContactSerializer
 
-class ContactViewsets(viewsets.ModelViewSet):
+class ContactViewsets(ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-    
+    permission_classes = [AllowAny]
